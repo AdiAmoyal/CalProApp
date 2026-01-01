4fbf5618d5eea12f116bbc629fb49ea65e3d6fca
AdiAmoyal <adi.amoyal02@gmail.com>
2026-01-01 21:53:11 +0200

    Initial Commit

---
+++ CalProApp/CalProAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalProAppApp.swift
+//  CalProApp
+//
+//  Created by Adi Amoyal on 01/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct CalProAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
